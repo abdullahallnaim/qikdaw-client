be287aad38745c650809d842b4990347cd1a1557
iamfazal <aghafazal5@gmail.com>
2021-04-22 15:09:24 +0500

    build for prod

--- src/Components/AllLogin/firebaseconfig.js
+++
@@ -1,9 +0,0 @@
-const firebaseconfig = {
-    apiKey: "AIzaSyAzQ_OAkHvB1sPOJFgw_Ma8FyMpB9WEMlA",
-    authDomain: "book-mark0.firebaseapp.com",
-    projectId: "book-mark0",
-    storageBucket: "book-mark0.appspot.com",
-    messagingSenderId: "135511543951",
-    appId: "1:135511543951:web:01fbab4f03d5076f0060fa"
-  };
-  export default firebaseconfig
